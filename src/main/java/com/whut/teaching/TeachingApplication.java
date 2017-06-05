70f9750ad0d29cfcbb713b29dca6f817e01eeb11
1806842229@qq.com <wpcid961015>
2017-06-05 13:21:08 +0800

    修改为打包为war包

--- src/main/java/com/whut/teaching/TeachingApplication.java
+++ src/main/java/com/whut/teaching/TeachingApplication.java
@@ -2,6 +2,7 @@ package com.whut.teaching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -9,7 +10,25 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.whut.teaching.dao")
 public class TeachingApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeachingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TeachingApplication.class, args);
+    }
+
+    /**
+     * 使用   war 打包时使用
+     * <p>
+     * Configure the application. Normally all you would need to do it add sources (e.g.
+     * config classes) because other settings have sensible defaults. You might choose
+     * (for instance) to add default command line arguments, or set an active Spring
+     * profile.
+     *
+     * @param builder a builder for the application context
+     * @return the application builder
+     * @see SpringApplicationBuilder
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TeachingApplication.class);
+    }
+
 }
